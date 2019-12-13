a3ae4af4d94fb922964c96fa1f942e1508797e79
ClemensScherzer <clemens.scherzer@gmx.at>
2019-12-13 11:12:45 +0000

    functions added

--- GPIO_G3/function.h
+++ GPIO_G3/function.h
@@ -1,10 +1,31 @@
 #ifndef GPIO_G3_HEADER_H
 #define GPIO_G3_HEADER_H
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define IN  0
+#define OUT 1
+
+#define LOW  0
+#define HIGH 1
+
+#define GREEN  4
+#define RED 17
+
 #include <wiringPi.h>
 #include <time.h>
-#include <stdlib.h>
 #include "function.c"
+
+static int GPIOExport(int);
+static int GPIOUnexport(int);
+static int GPIODirection(int, int);
+static int GPIORead(int);
+static int GPIOWrite(int, int);
 
 void ledBlinkAll20WiringPi();
 void ledBlinkRed5WiringPi();
--- GPIO_G3/main.c
+++ GPIO_G3/main.c
@@ -1,14 +1,53 @@
-
 #include "function.h"
 
 
-#define GREEN 0
-#define RED 7
-
-int main(){
+int main(int argc, char *argv[]){
 	//ledBlinkAll20WiringPi();
 	//ledBlinkRed5WiringPi();
 	//ledBlinkGreen5WiringPi();
-	ledBlinkRandomWiringPi();
-	return 0;
+	//ledBlinkRandomWiringPi();
+	//return 0;
+
+
+	/*
+	 * Enable GPIO pins
+	 */
+	if (-1 == GPIOExport(GREEN) || -1 == GPIOExport(RED))		
+		return(1);
+
+	/*
+	 * Set GPIO directions
+	 */
+	if (-1 == GPIODirection(GREEN, OUT) || -1 == GPIODirection(RED, OUT))
+		return(2);
+
+	do {
+		/*
+		 * Write GPIO value
+		 */
+		if(-1 == GPIOWrite(GREEN, LOW))
+			return(3);
+		if(-1 == GPIOWrite(RED, LOW))
+			return(3);
+
+		sleep(1/atoi(argv[3])/2);		
+	
+		if (-1 == GPIOWrite(GREEN, HIGH))
+			return(3);
+		if(-1 == GPIOWrite(RED, HIGH))
+			return(3);
+
+		sleep(1/atoi(argv[3])/2);
+		//sleep(1/atoi(argv[3])/2);
+
+		//sleep(1/atoi(argv[3])/2);
+	}while (0==0);
+
+	/*
+	 * Disable GPIO pins
+	 */
+	if (-1 == GPIOUnexport(GREEN) || -1 == GPIOUnexport(RED))
+		return(4);
+
+	return(0);
 }
